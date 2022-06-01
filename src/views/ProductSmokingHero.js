f64a8808d6cf0daef5dbb01730e78cc2d1b14ca2
ejohnguia <35359631+ejohnguia@users.noreply.github.com>
2022-05-31 18:49:25 -0600

    edit email and inquiries section

--- src/views/ProductSmokingHero.js
+++ src/views/ProductSmokingHero.js
@@ -1,39 +1,43 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Typography from '../components/Typography';
+/** @format */
+
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Typography from "../components/Typography";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
 
 function ProductSmokingHero() {
-  return (
-    <Container
-      component="section"
-      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', my: 9 }}
-    >
-      <Button
-        sx={{
-          border: '4px solid currentColor',
-          borderRadius: 0,
-          height: 'auto',
-          py: 2,
-          px: 5,
-        }}
-      >
-        <Typography variant="h4" component="span">
-          Got any questions? Need help?
-        </Typography>
-      </Button>
-      <Typography variant="subtitle1" sx={{ my: 3 }}>
-        We are here to help. Get in touch!
-      </Typography>
-      <Box
-        component="img"
-        src="/static/themes/onepirate/producBuoy.svg"
-        alt="buoy"
-        sx={{ width: 60 }}
-      />
-    </Container>
-  );
+	return (
+		<Container
+			component="section"
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				my: 9,
+			}}
+		>
+			<Button
+				sx={{
+					border: "4px solid currentColor",
+					borderRadius: 0,
+					height: "auto",
+					py: 2,
+					px: 5,
+				}}
+			>
+				<Typography variant="h4" component="span">
+					Got any questions? Inquiries?
+				</Typography>
+			</Button>
+			<Typography variant="subtitle1" sx={{ my: 3 }}>
+				Get in touch with us and we'll get back to you as soon as
+				possible.
+			</Typography>
+			<QuestionAnswerIcon color="primary" />
+		</Container>
+	);
 }
 
 export default ProductSmokingHero;
