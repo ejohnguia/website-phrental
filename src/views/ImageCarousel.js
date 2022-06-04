8f5b180dbd40c5c4e71ac8cd43f36a6a75e62518
ejohnguia <35359631+ejohnguia@users.noreply.github.com>
2022-06-04 17:57:03 -0600

    add dummy images

--- src/views/ImageCarousel.js
+++ src/views/ImageCarousel.js
@@ -17,18 +17,42 @@ import "../styles/swiperStyle.css";
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
 
-function Temp() {
+function ImageCarousel() {
+	const images = {
+		parking:
+			"https://images.unsplash.com/photo-1621929747188-0b4dc28498d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
+		view: "https://images.unsplash.com/photo-1645623457286-4c3bac259bde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+		frontyard:
+			"https://images.unsplash.com/photo-1587716839025-07c325222549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+		livingRoom:
+			"https://images.unsplash.com/photo-1567767292278-a4f21aa2d36e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+		bedroom:
+			"https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+		bedroom2:
+			"https://images.unsplash.com/photo-1616486029423-aaa4789e8c9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
+		bathroom:
+			"https://images.unsplash.com/photo-1604709177225-055f99402ea3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+		kitchen:
+			"https://images.unsplash.com/photo-1556912173-3bb406ef7e77?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+		diningRoom:
+			"https://images.unsplash.com/photo-1617806118233-18e1de247200?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
+		laundryRoom:
+			"https://images.unsplash.com/photo-1604335398980-ededcadcc37d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+		backyard:
+			"https://images.unsplash.com/photo-1560749003-f4b1e17e2dff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+	};
+
 	return (
 		<Box
 			component="section"
 			sx={{
 				display: "flex",
 				overflow: "hidden",
-				bgcolor: "black",
+				bgcolor: "white",
 			}}
 		>
 			<Container
-				sx={{ mt: 15, mb: 30, display: "flex", position: "relative" }}
+				sx={{ mt: 0, mb: 0, display: "flex", position: "relative" }}
 			>
 				<Swiper
 					spaceBetween={30}
@@ -44,19 +68,19 @@ function Temp() {
 					modules={[Autoplay, Pagination, Navigation]}
 					className="mySwiper"
 				>
-					<SwiperSlide>Slide 1</SwiperSlide>
-					<SwiperSlide>Slide 2</SwiperSlide>
-					<SwiperSlide>Slide 3</SwiperSlide>
-					<SwiperSlide>Slide 4</SwiperSlide>
-					<SwiperSlide>Slide 5</SwiperSlide>
-					<SwiperSlide>Slide 6</SwiperSlide>
-					<SwiperSlide>Slide 7</SwiperSlide>
-					<SwiperSlide>Slide 8</SwiperSlide>
-					<SwiperSlide>Slide 9</SwiperSlide>
+					{Object.keys(images).map((key) => (
+						<SwiperSlide key={key}>
+							<img
+								src={images[key]}
+								alt={key}
+								style={{ maxHeight: 500 }}
+							/>
+						</SwiperSlide>
+					))}
 				</Swiper>
 			</Container>
 		</Box>
 	);
 }
 
-export default Temp;
+export default ImageCarousel;
