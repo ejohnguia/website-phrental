96cb6b9d0afffe49387196c4bfd63197d5486616
ejohnguia <35359631+ejohnguia@users.noreply.github.com>
2022-06-04 10:29:43 -0600

    add product values

--- src/views/ProductValues.js
+++ src/views/ProductValues.js
@@ -1,94 +1,89 @@
-import * as React from 'react';
+/** @format */
 
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import Typography from '../components/Typography';
+import * as React from "react";
+
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Typography from "../components/Typography";
+
+// Icons
+import LandscapeIcon from "@mui/icons-material/Landscape";
+import CorporateFareIcon from "@mui/icons-material/CorporateFare";
+import DirectionsCarFilledIcon from "@mui/icons-material/DirectionsCarFilled";
 
 const item = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  px: 5,
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+	px: 5,
 };
 
 function ProductValues() {
-  return (
-    <Box
-      component="section"
-      sx={{ display: 'flex', overflow: 'hidden', bgcolor: 'secondary.light' }}
-    >
-      <Container sx={{ mt: 15, mb: 30, display: 'flex', position: 'relative' }}>
-        <Box
-          component="img"
-          src="/static/themes/onepirate/productCurvyLines.png"
-          alt="curvy lines"
-          sx={{ pointerEvents: 'none', position: 'absolute', top: -180 }}
-        />
-        <Grid container spacing={5}>
-          <Grid item xs={12} md={4}>
-            <Box sx={item}>
-              <Box
-                component="img"
-                src="/static/themes/onepirate/productValues1.svg"
-                alt="suitcase"
-                sx={{ height: 55 }}
-              />
-              <Typography variant="h6" sx={{ my: 5 }}>
-                The best luxury hotels
-              </Typography>
-              <Typography variant="h5">
-                {
-                  'From the latest trendy boutique hotel to the iconic palace with XXL pool'
-                }
-
-                {
-                  ', go for a mini-vacation just a few subway stops away from your home.'
-                }
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Box sx={item}>
-              <Box
-                component="img"
-                src="/static/themes/onepirate/productValues2.svg"
-                alt="graph"
-                sx={{ height: 55 }}
-              />
-              <Typography variant="h6" sx={{ my: 5 }}>
-                New experiences
-              </Typography>
-              <Typography variant="h5">
-                {
-                  'Privatize a pool, take a Japanese bath or wake up in 900m2 of garden… '
-                }
-
-                {'your Sundays will not be alike.'}
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Box sx={item}>
-              <Box
-                component="img"
-                src="/static/themes/onepirate/productValues3.svg"
-                alt="clock"
-                sx={{ height: 55 }}
-              />
-              <Typography variant="h6" sx={{ my: 5 }}>
-                Exclusive rates
-              </Typography>
-              <Typography variant="h5">
-                {'By registering, you will access specially negotiated rates '}
-                {'that you will not find anywhere else.'}
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  );
+	return (
+		<Box
+			component="section"
+			sx={{
+				display: "flex",
+				overflow: "hidden",
+				bgcolor: "secondary.light",
+			}}
+		>
+			<Container
+				sx={{ mt: 10, mb: 10, display: "flex", position: "relative" }}
+			>
+				<Box
+					component="img"
+					src="/static/themes/onepirate/productCurvyLines.png"
+					alt="curvy lines"
+					sx={{
+						pointerEvents: "none",
+						position: "absolute",
+						top: -180,
+					}}
+				/>
+				<Grid container spacing={5}>
+					<Grid item xs={12} md={4}>
+						<Box sx={item}>
+							<LandscapeIcon fontSize="large" />
+							<Typography variant="h6" sx={{ my: 5 }}>
+								Overlooking View
+							</Typography>
+							<Typography variant="h5">
+								{
+									"From up on the hill, you'll be able to see all of Morong."
+								}
+							</Typography>
+						</Box>
+					</Grid>
+					<Grid item xs={12} md={4}>
+						<Box sx={item}>
+							<CorporateFareIcon fontSize="large" />
+							<Typography variant="h6" sx={{ my: 5 }}>
+								Aspiring Location
+							</Typography>
+							<Typography variant="h5">
+								{
+									"Future location for the new university as well as the new hospital."
+								}
+							</Typography>
+						</Box>
+					</Grid>
+					<Grid item xs={12} md={4}>
+						<Box sx={item}>
+							<DirectionsCarFilledIcon fontSize="large" />
+							<Typography variant="h6" sx={{ my: 5 }}>
+								Parking Included
+							</Typography>
+							<Typography variant="h5">
+								{"One outdoor vehicle parking space."}
+							</Typography>
+						</Box>
+					</Grid>
+				</Grid>
+			</Container>
+		</Box>
+	);
 }
 
 export default ProductValues;
