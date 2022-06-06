9220147a0af5c78ffbc5e967be7c97d6f7b0b1aa
ejohnguia <35359631+ejohnguia@users.noreply.github.com>
2022-06-06 10:25:54 -0600

    undo Morong removal

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import Morong from "./pages/Morong";
 import Rentals from "./pages/Rentals";
 import Error from "./pages/Error";
 
@@ -12,6 +13,7 @@ function App() {
 		<>
 			<Routes>
 				<Route exact path="/" element={<Home />} />
+				<Route exact path="/morong" element={<Morong />} />
 				<Route exact path="/rentals" element={<Rentals />} />
 				<Route exact path="/contact" element={<Contact />} />
 				<Route path="*" element={<Error />} />
